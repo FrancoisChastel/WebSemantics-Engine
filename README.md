a518725d454b7250c89b9a5c5ab375725047678c
Antoine B <antoine.breton@insa-lyon.fr>
2016-11-04 11:57:27 +0100

    Modification - README added apikey

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 
 You need to get an custom search API, key with your google account on https://console.developers.google.com/apis/.
 And to instanciate a search engine on your account's control panel. 
+
+# API keys
+! Careful : there is a request limit per day on certain API. If you want to debug something that required a lof of request, you shall get your own key.
+
+Antoine Breton api keys :
+ - Google Custom Search : AIzaSyB55Q4tpXmOmXrRaYU2MC7IvfGRn-VQa6c
+ - 
+
--- engine/engine.sh
+++ engine/engine.sh
@@ -2,14 +2,14 @@
 
 # Parsing parameters
 InputRequest=$1
-ApiKey=AIzaSyB55Q4tpXmOmXrRaYU2MC7IvfGRn-VQa6c 
+ApiKeyCustomSearch=AIzaSyB55Q4tpXmOmXrRaYU2MC7IvfGRn-VQa6c 
 cx=008214837905171502064:yuy4kpi9ui0
 
 
 echo Request : $InputRequest
 
 # STEP 1: Send Google Request
-curl "https://www.googleapis.com/customsearch/v1?q=$InputRequest&key=$ApiKey&cx=$cx" > Responses/customSearchResponse 
+curl "https://www.googleapis.com/customsearch/v1?q=$InputRequest&key=$ApiKeyCustomSearch&cx=$cx" > Responses/customSearchResponse 
 cat Responses/customSearchResponse | jq -r .items[].link > Responses/URLs
 
 # STEP 2: Send to AlchimyAPI
