887ae5e6f55c2bbcb9acb645a780f69f8e145850
Antoine B <antoine.breton@insa-lyon.fr>
2016-11-07 17:42:10 +0100

    Externalization of Api Variables

--- engine/engine.sh
+++ engine/engine.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 # Parsing parameters
+if [ -z ${1+x} ]; 
+	then 
+		echo "Please specify à query. Example : ./engine.sh \"Kurt Cobain\""; 
+		exit 1;
+fi
+
 InputRequest=$1
-ApiKeyCustomSearch=AIzaSyB55Q4tpXmOmXrRaYU2MC7IvfGRn-VQa6c 
-cx=008214837905171502064:yuy4kpi9ui0
 
 
-echo Request : $InputRequest
+# Defining variables
+ApiKeyCustomSearch=`cat keys | jq -r .customSearch.apiKey`
+cx=`cat keys | jq -r .customSearch.cx`
+ApiKeyAlchemy=`cat keys | jq -r .alchemy.apiKey`
+
 
 # STEP 1: Send Google Request
 curl "https://www.googleapis.com/customsearch/v1?q=$InputRequest&key=$ApiKeyCustomSearch&cx=$cx" > Responses/customSearchResponse 
 cat Responses/customSearchResponse | jq -r .items[].link > Responses/URLs
 
-# STEP 2: Send to AlchimyAPI
+
+# STEP 2: Send to AlchimyAPI. To extract things
+for line in $(cat Responses/URLs);
+do 
+	curl -X POST -d "outputMode=json" -d "extract=entities,keywords" -d "sentiment=1" -d "maxRetreve=1" -d "$line" "https://gateway-a.watsonplatform.net/calls/url/URLGetCombinedData?apikey=$ApiKeyAlchemy">>Responses/AlchemyResponse; 
+done  
 
